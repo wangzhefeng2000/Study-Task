136d7e16f5c94fcb1435a69cebf172c3db82195a
Frank <wangzhefeng2000@163.com>
2016-11-01 14:46:28 +0800

    添加项目文件。

---
+++ studyTask2/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace studyTask2
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ViewModel _viewModel;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _viewModel = new ViewModel();
+            DataContext = _viewModel;
+        }
+
+        private CancellationTokenSource cts = new CancellationTokenSource();
+        private void StartClick(object sender, RoutedEventArgs e)
+        {
+            StartTask();
+        }
+
+        private void EndClick(object sender, RoutedEventArgs e)
+        {
+            EndTask();
+        }
+
+        async void StartTask()
+        {
+            StartBtn.IsEnabled = false;
+            EndBtn.IsEnabled = true;
+           
+            
+
+             var progress = new Progress<ViewModel>();
+            progress.ProgressChanged += (s, e) =>
+            {
+                _viewModel.Step = e.Step ;
+                _viewModel.Message = e.Message;
+            };
+            await MyTaskAsync(progress, cts);
+
+            StartBtn.IsEnabled = true;
+            EndBtn.IsEnabled = false;
+        }
+
+        private static async Task MyTaskAsync(IProgress<ViewModel> progress, CancellationTokenSource cts)
+        {
+            Func<Task> taskFunc = () => Task.Run(() =>
+            {
+                for (var i = 1; i <= 500 && !cts.IsCancellationRequested; i++)
+                {
+                    Thread.Sleep( i);
+                    progress.Report(new ViewModel { Step = i / 5, Message = "Message " + i });
+                }
+            });
+
+            await taskFunc();
+        }
+
+
+        private void EndTask()
+        {
+            cts.Cancel();
+            MessageBox.Show("Canceled!");
+        }
+    }
+}
---
+++ studyTask2/ViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Commands;
+using Prism.Mvvm;
+
+namespace studyTask2
+{
+    class ViewModel : BindableBase
+    {
+        private int _step;
+
+        public int Step
+        {
+            get { return _step; }
+            set { SetProperty(ref _step, value); }
+        }
+
+        private string _message;
+
+        public string Message
+        {
+            get { return _message;}
+            set { SetProperty(ref _message, value); }
+        }
+    }
+}
